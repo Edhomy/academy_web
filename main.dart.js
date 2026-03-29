ee3febc6cb532d3a45e78b7d3c977366760db47f
Cristian A. Jara Gutierrez <cristian.jara@edhomy.com>
2026-03-29 18:58:23 -0300

    Add files via upload

--- main.dart.js
+++ main.dart.js
@@ -32548,7 +32548,7 @@ $S:2}
 A.aRV.prototype={
 $1(a){var s=A.ed().b
 s=s==null?null:s.canvasKitBaseUrl
-return(s==null?"https://www.gstatic.com/flutter-canvaskit/052f31d115eceda8cbff1b3481fcde4330c4ae12/":s)+a},
+return(s==null?"https://www.gstatic.com/flutter-canvaskit/425cfb54d01a9472b3e81d9e76fd63a4a44cfbcb/":s)+a},
 $S:20}
 A.Qh.prototype={
 a22(){var s=this.akI(),r=$.bl.bN().ImageFilter.MakeColorFilter(s,null)
