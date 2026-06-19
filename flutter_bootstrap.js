1413a56309e4678325611781e474aeb95262ff81
Cristian A. Jara Gutierrez <cristian.jara@edhomy.com>
2026-06-19 01:00:57 -0400

    Add files via upload

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"77e2e94772b6eb43759e34ed1ad7da4674e19c
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "107360022" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3585844510" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
